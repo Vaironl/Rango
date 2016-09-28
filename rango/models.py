a3e1df7c677c4078a4f18a267c181799f46b3c53
vaironl <vairon@live.com>
2016-09-28 19:40:20 -0400

    Working on data driven pages

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
@@ -6,14 +7,19 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
-    def save(self):
-        #Check if the views is negative and set it to 0
+    def save(self, *args, **kwargs):
+        # Check if the views is negative and set it to 0
         if (self.views < 0):
             self.views = 0
+
+        self.slug = slugify(self.name)
+
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 
 # Create your views here.
@@ -17,6 +17,34 @@ def index(request):
 
     # return rendered response
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    # Create context dictionary which we can pass to the rendering engine
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all associated pages
+        pages = Page.objects.filter(category=category)
+
+        # Add our result list to the template context under the name pages
+        context_dict['pages'] = pages
+        # Also add the category object from the database
+        # to the context dictionary.
+        # We'll use this in the template to verify the category
+        # Exists
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # If we did not find the specified category don't do anything
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Render the response and return it to the client
+    return render(request, 'rango/category.html', context_dict)
 
 
 def about(request):
